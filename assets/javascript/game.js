9e6f1ea3394e7ef8b189f5233c78ade417a9abf7
Christopher Demeke <christopherdemeke@dmk.attlocal.net>
2018-02-13 19:02:34 -0600

    Full Draft of hangman game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,109 @@
-
-
-
-// Need an array of all possible letters to choose from
+// Superhero Hangman Game
+// ==================================================================
+// Variables
+var randomWord;
+var wins = 0;
+var guesses = 10;
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Need an array of words that computer chooses at random to be the puzzle user is guessing.
-var superheros = ["superman", "batman", "wonderwoman", "hulk", "ironman", "black panther", "captain america", "green lantern", "flash", "green arrow", "thor"];
-console.log("superhero array has " + superheros.length + " words");
-console.log(alphabet.length);
-
-var randomWord = superheros[Math.floor(Math.random() * superheros.length)];
-// randomWord = randomWord.replace(/\s/g, "-"); // replaing spaces with dash
-console.log("word is " + randomWord);
-console.log(typeof randomWord);
-console.log(randomWord.length);
-var blankSpaces = [];
-var letterSpace = randomWord.split("");
-console.log(letterSpace);
-console.log(typeof letterSpace);
-console.log(letterSpace[3]);
-
-/* set up display of letters in current word */
-document.getElementById("letters").value = '';
-letters = document.getElementById("letters");
-for (var i = 0; i < randomWord.length; i++) {
-    blankSpaces[i] = "_ ";
-    // letters.innerHTML = blankSpaces;
-    var letters = document.getElementById("letters");
-    var blanks = document.createTextNode(blankSpaces[i]);
-    letters.appendChild(blanks);
+var superhero = ["superman", "hulk", "flash", "thor", "batman", "magneto", "deadpool", "storm", "wolverine", "wonderwoman", "hawkeye", "daredevil"];
+var wrongLetter = [];
+var blankSpace = [];
+var pageblankSpace = document.getElementsByClassName('blankSpace');
+var pageWrongGuess = document.getElementsByClassName('wrongGuess');
+// ==================================================================
+// Functions
+// function to select a word randomly from the list at random
+var newWord = () => {
+    randomWord = superhero[Math.floor(Math.random() * superhero.length)];
+    return randomWord;
 }
-console.log(blankSpaces);
+// function to greate the blank spaces
+var generateblankSpace = () => {
+    for (var i = 0; i < randomWord.length; i++) {
+        blankSpace.push('_ ');
+    }
+    return blankSpace;
+}
+// function to clear the blank spaces, to be used onece a game is finished.
+var clearblankSpace = () => {
+    blankSpace = [];
+}
+// function to reset the guess counter
+var resetGuessCount = () => {
+    guesses = 10;
+}
+// function to reset the wrong letter collection box
+var resetWrongLetters = () => {
+    wrongLetter = [];
+}
+// ==================================================================
+// ==================================================================
+//Game Play
+// ==================================================================
+// select a random word from the list
+newWord();
+// create the blank spaces for the word on the screen
+generateblankSpace(); {
+    pageblankSpace[0].innerHTML = blankSpace.join('');
+}
+// recive user's letter guess
+document.onkeyup = function (event) {
+    // Determines which key was pressed.
+    var userGuess = event.key;
+    var pageGuessCount = document.getElementById("guessCount");
+    var pageWinCount = document.getElementById('winCount');
+    console.log(randomWord);
 
+    //Check if userGuess is valid character for the game
+    if (alphabet.indexOf(userGuess) > -1) {
+        pageWinCount.innerHTML = wins ;
+        // If user guesses the correct letter
+        if (randomWord.indexOf(userGuess) > -1) {
+            // replace blankSpace with correct letter 1
+            blankSpace[randomWord.indexOf(userGuess)] = userGuess;
+            pageblankSpace[0].innerHTML = blankSpace.join('');
+            // replace blankSpace with correct letter in case there are two of same letter
+            var check2 = randomWord.indexOf(userGuess) + 1;
+            blankSpace[randomWord.indexOf(userGuess, check2)] = userGuess;
+            pageblankSpace[0].innerHTML = blankSpace.join('');
+            if (blankSpace.join('') == randomWord) {
+                wins++;
+                // after a win game needs to reset with sequence of functions below
+                clearblankSpace();
+                newWord();
+                generateblankSpace(); {
+                    pageblankSpace[0].innerHTML = blankSpace.join('');
+                }
+                resetGuessCount();
+                resetWrongLetters();
+                pageWinCount.innerHTML = wins ;
+                pageGuessCount.innerHTML = "You have " + guesses + " left!";
+                pageWrongGuess[0].innerHTML = wrongLetter;
+            }
+        } else {
+            // wrong letter must subtract a guess
+            guesses--;
+            // check to see if wrong letter has been pressed before, if not place in worng guess box
+            if (wrongLetter.indexOf(userGuess) < 0) {
+                wrongLetter.push(userGuess);
+                pageWrongGuess[0].innerHTML = wrongLetter;
+                pageGuessCount.innerHTML = "You have " + guesses + " left!"
 
-
-
-
-guesses = 5;
-guessesRem = document.getElementById("guessesRem")
-guessesRem.innerHTML = 'You have ' + guesses + ' gueses remaining';
-
-
-
-
-
+                if (guesses == 0) {
+                    // after game over game is reset after an alert
+                    clearblankSpace();
+                    newWord();
+                    generateblankSpace(); {
+                        pageblankSpace[0].innerHTML = blankSpace.join('');
+                    }
+                    resetGuessCount();
+                    resetWrongLetters();
+                    pageWinCount.innerHTML = wins ;
+                    pageGuessCount.innerHTML = "You have " + guesses + " left!";
+                    pageWrongGuess[0].innerHTML = wrongLetter;
+                    alert("Game Over")
+                }
+            }
+        }
+    }
+}
